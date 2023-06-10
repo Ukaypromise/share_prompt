b37ec2359e2ffb1f5be71fce15d15a0a80226890
Ukaypromise <promiseukaimo@gmail.com>
2023-06-10 12:38:57 +0100

    Setup Authentication

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import { connectToDatabase } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
@@ -9,20 +10,31 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session
+  },
+
   async signIn({ profile }) {
     try {
-     await connectToDatabase()
-    //  Check if a user already exist
-
-
-    // If no user, then create a new user
-    } catch (err) {
+      await connectToDatabase();
+      //  Check if a user already exist
+      const userExists = await User.findOne({ email: profile.email });
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+    } catch (error) {
       console.log(err);
       return false;
-      
     }
   },
 });
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }; 
