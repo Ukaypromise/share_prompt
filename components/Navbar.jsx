014f44c5b9569c50499d7f9b6f07892877defaeb
Ukaypromise <promiseukaimo@gmail.com>
2023-06-10 12:43:06 +0100

    Update Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Navbar = () => {
-  const isUserLoggedIn = true;
+const { data:session} = useSession()
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
 
@@ -32,7 +32,7 @@ const Navbar = () => {
       </Link>
       {/* Desktop Nav */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -69,7 +69,7 @@ const Navbar = () => {
       </div>
       {/* Mobile Nav */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
               src="/assets/images/logo.svg"
